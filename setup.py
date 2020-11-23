fd87d853ae2812cea883361d2932e706152234f4
Zoltan Kozma <zoltan@zoltankozma.com>
2020-11-23 05:08:03 +0000

    making it work for windows now

--- setup.py
+++ setup.py
@@ -41,11 +41,14 @@ skbuild.setup(
     },
     cmake_args=[
         "-DECL_VERSION=" + version,
-        "-DBUILD_APPLICATIONS=ON",
+        # "-DBUILD_APPLICATIONS=ON",
+        "-DENABLE_PYTHON=ON",
+        "-DBUILD_SHARED_LIBS"
         "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON",
-        "-DCMAKE_INSTALL_BINDIR=python/ecl/.bin",
-        "-DCMAKE_INSTALL_LIBDIR=python/ecl/.libs",
-        "-DCMAKE_INSTALL_INCLUDEDIR=python/ecl/.include",
+        "-DCMAKE_BUILD_TYPE=Release",
+        # "-DCMAKE_INSTALL_BINDIR=python/ecl/.bin",
+        # "-DCMAKE_INSTALL_LIBDIR=python/ecl/.libs",
+        # "-DCMAKE_INSTALL_INCLUDEDIR=python/ecl/.include",
         # we can safely pass OSX_DEPLOYMENT_TARGET as it's ignored on
         # everything not OS X. We depend on C++11, which makes our minimum
         # supported OS X release 10.9
